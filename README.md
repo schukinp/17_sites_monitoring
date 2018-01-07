5c24f61865a1275cb191b72bc5e4b9913b3efa31
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-07 14:22:17 +0300

    Script is created

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script tracks the status of url by following criteria:
+* url response is 200
+* domain expiration date is more than [number of days] from now
+
+# Status logic
+
+URL is OK! if:
+* the response is 200
+* the domain expiration date is more than [number of days] from now
+
+URL is not OK! if either:
+* response in not 200
+OR URL doesn't respond
+* the domain expiration date is not more than [number of days] from now
+OR impossible to get expiration date
+OR URL doesn't respond
+
+# To run the code
+
+* Put your file into root script folder
+* Run check_sites_health.py [file name.extension] [number of days]
+
+Example of script launch on Linux, Python 3.5:
+```
+$ python check_sites_health.py urls.txt 30
+http://www.yandex.ru is OK!
+http://www.vk.ru is OK!
+http://www.rbc.ru is OK!
+http://www.xyz123.com.ua is not OK!
+```
+Windows launches the same
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,47 @@
+import requests
+from whois import whois
+from datetime import datetime, timedelta
+import sys
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as file:
+        return file.read().split()
 
-def is_server_respond_with_200(url):
-    pass
 
-def get_domain_expiration_date(domain_name):
-    pass
+def is_server_respond_with_200(file):
+    response = []
+    for url in file:
+        try:
+            response.append(requests.get(url).status_code == 200)
+        except ValueError:
+            response.append('url don\'t respond')
+    return response
+
+
+def get_domain_expiration_date(file, days):
+    response = []
+    for url in file:
+        try:
+            domain = whois(url)
+            response.append((domain.expiration_date + timedelta(days=days)) > datetime.now())
+        except ValueError:
+            response.append('url doesn\'t respond')
+    return response
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        path = sys.argv[1]
+        days = int(sys.argv[2])
+        urls_list = load_urls4check(path)
+        for key, value in dict(
+           zip(urls_list,
+               zip(is_server_respond_with_200(urls_list),
+                   get_domain_expiration_date(urls_list, days)))).items():
+            if value == (True, True):
+                print('{} is OK!'.format(key))
+            else:
+                print('{} is not OK!'.format(key))
+    except (FileNotFoundError, ValueError, IndexError):
+        print('Cannot read file or missing one of the arguments')
